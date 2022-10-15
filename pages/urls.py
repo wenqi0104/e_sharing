d85675e31a32d6d6c21a8cddba1a131eeed8844a
Coldglow <wswchange@gmail.com>
2022-10-15 18:26:02 +0100

    数据库合并

--- pages/urls.py
+++ pages/urls.py
@@ -12,7 +12,9 @@ from e_portal import views as pviews
 urlpatterns = [
     # 这里的index是定义在views里的index方法名
     # 填空就是默认进入的页面
-    path('', pviews.test),
+    path('', views.index, name='index'),
+    path("login/", Login.as_view(), name='login'),
+    path("register/", Register.as_view(), name='register')
 ]
 
 
