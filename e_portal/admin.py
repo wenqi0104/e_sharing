8575ebc1b3df3bce28fec3f7bccc66e3a24d6eac
Charles <1092637484@qq.com>
2022-10-16 11:12:46 +0100

    admin register

--- e_portal/admin.py
+++ e_portal/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Customers, Operators, Vehicles, OperationsHistory, Payments, TopUpHistory, RepairHistory
 
 # Register your models here.
+admin.site.register(Customers)
+admin.site.register(Operators)
+admin.site.register(Vehicles)
+admin.site.register(OperationsHistory)
+admin.site.register(Payments)
+admin.site.register(TopUpHistory)
+admin.site.register(RepairHistory)
