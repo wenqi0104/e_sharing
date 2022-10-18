95eb97e23afa0085e510abc6709e167d4d9194bd
wenqi0104 <1299226721@qq.com>
2022-10-18 13:39:44 +0100

    feat: 更改默认页面逻辑

--- e_portal/urls.py
+++ e_portal/urls.py
@@ -5,8 +5,9 @@ from django.urls import path
 app_name = 'e_portal'
 urlpatterns = [
     #log
-    path('', log.index, name='index'),
-    path("login/", log.login, name='login'),
+    # path('', log.index, name='index'),
+    path('', log.login, name='login'),
+    path("main/", log.index, name='index'),
     path("register/", log.register, name='register'),
     path("pwd_reset/", log.pwd_reset, name='pwd_reset'),
     path("error404/", log.Error404, name='error404'),
