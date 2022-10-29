82569afd77216867bdea3dbe632d1e2765fc3e86
wenqi0104 <1299226721@qq.com>
2022-10-29 20:26:56 +0100

    feat: modify message popup; modify logout function;

--- e_portal/urls.py
+++ e_portal/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path,include
+from django.urls import path, include
 from .views import log, customer, manager, operator
 from django.urls import path
 
 app_name = 'e_portal'
 urlpatterns = [
-    #log
+    # log
     # path('', log.index, name='index'),
     path('', log.login, name='login'),
     path("main/", log.index, name='index'),
@@ -12,20 +12,16 @@ urlpatterns = [
     path("pwd_reset/", log.pwd_reset, name='pwd_reset'),
     path("error404/", log.Error404, name='error404'),
 
-    #customer
+    # customer
     path("vehicles/", customer.getAvailableVehicles, name='vehicles_list'),
     # 前端调试使用
     path("vehicles/1/", customer.getVehicleDetails, name='vehicles_detail'),
-    # path("vehicles/<int:vehicles_id>/", customer.getVehicleDetails, name='vehicles_detail'),
+    path("vehicles/<int:vehicles_id>/", customer.getVehicleDetails, name='vehicles_detail'),
+    path("rent", customer.getVehicleDetails, name='vehicles_detail'),
     path("rents", customer.rentToOrder, name='rents'),
 
+    # operator
 
-
-    #operator
-
-
-
-    #manager
-
+    # manager
 
 ]
