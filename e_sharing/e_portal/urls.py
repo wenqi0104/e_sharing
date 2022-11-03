05e77431799412d48b2ebf9acb992f6ed47565a4
wenqi0104 <1299226721@qq.com>
2022-11-03 12:24:45 +0000

    modify vehicles_detail.html

---
+++ e_sharing/e_portal/urls.py
@@ -0,0 +1,49 @@
+from django.urls import path, include
+from .views import log, customer, manager, operator
+from django.urls import path
+
+app_name = 'e_portal'
+urlpatterns = [
+    # log
+    # path('', log.index, name='index'),
+    path('', log.login, name='login'),
+    path("main/", log.index, name='index'),
+    path("register/", log.register, name='register'),
+    path("pwd_reset/", log.pwd_reset, name='pwd_reset'),
+    path("error404/", log.Error404, name='error404'),
+
+    # customer
+    path("vehicles/", customer.getAvailableVehicles, name='vehicles_list'),
+    # 进入details页面执行的
+    path("vehicles/<int:vehicles_id>/", customer.getVehicleDetails, name='vehicles_detail'),
+    # 在details页面执行，rent a car
+    path("rent/<int:vehicles_id>/", customer.rent, name='rent'),
+    # customer pay
+    path("pay/<int:order_id>/", customer.pay, name='payment'),
+    # customer report a broken car
+    path("report/<int:order_id>/", customer.report, name='report'),
+    # return a car
+    path("return/<int:order_id>/", customer.returnVehicle, name='returnVehicle'),
+    # rental info page
+    path("rents/", customer.rents, name='rents'),
+    # payment history
+    path("history/",customer.getPaymentHistory, name='paymentHistory'),
+
+    # operator
+    path("operators/vehicles_using/", operator.getUsingVehicles, name='vehicles_using'),
+    path("operators/vehicles_available/", operator.getUnUsingVehicles, name='vehicles_available'),
+    path("operators/vehicles_deal/", operator.getDealVehicles, name='vehicles_deal'),
+    path("move/", operator.moveVehicles),
+    path("charge/", operator.chargeVehicles),
+    path("repair/", operator.repairVehicles),
+    path("getOperator/", operator.getOperator),
+    path("track/", operator.track),
+    # debugging
+    path("vehicles_using/", operator.getUsingVehicles),
+    path("vehicles_unusing/", operator.getUnUsingVehicles),
+    path("vehicles_move/", operator.moveVehicles),
+    path("vehicles_deal/", operator.getDealVehicles),
+
+    # manager
+    path("manager/", manager.graphPage)
+]
---
+++ e_sharing/e_portal/views/operator.py
@@ -0,0 +1,141 @@
+"""
+operator用户的所有接口
+"""
+import json
+import math
+import random
+import time
+
+import geocoder
+# from geocoder import arcgis
+# from django.core import serializers
+import requests
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+from .. import models
+
+
+# Create your views here.
+# base_log：经度基准点，
+# base_lat：维度基准点，
+# radius：距离基准点的半径
+def generate_random_gps(base_log=55.85806, base_lat=-4.25889, radius=100000):
+    radius_in_degrees = radius / 111300
+    u = float(random.uniform(0.0, 1.0))
+    v = float(random.uniform(0.0, 1.0))
+    w = radius_in_degrees * math.sqrt(u)
+    t = 2 * math.pi * v
+    x = w * math.cos(t)
+    y = w * math.sin(t)
+    longitude = y + base_log
+    latitude = x + base_lat
+    locName = geocoder.arcgis([longitude, latitude], method='reverse').address
+    longitude = '%.10f' % longitude
+    latitude = '%.10f' % latitude
+
+    return longitude, latitude, locName
+    # data = {"longitude": longitude, "latitude": latitude, "locName": locName}
+    # return JsonResponse(data)
+
+
+def moveVehicles(request):
+    # if request.method == "GET":
+    #     vid = request.GET.get("vid")
+    #     longitude, latitude = generate_random_gps()
+    #     models.Vehicles.objects.filter(id=vid).update(longitude=longitude, latitude=latitude)
+    #     return HttpResponse("success")
+
+    if request.method == "POST":
+        vid = request.POST.get("vid")
+        oid = request.session.get('oid')
+        longitude, latitude, locName = generate_random_gps()
+        models.Vehicles.objects.filter(id=vid).update(longitude=longitude, latitude=latitude, locName=locName)
+        models.OperationsHistory.objects.create(operationType='move', oid=oid, vid=vid,
+                                                operateTime=time.strftime('%Y-%m-%d %H:%M:%S'))
+        data = {"success_msg": "success"}
+        return JsonResponse(data)
+
+
+def getUsingVehicles(request):
+    """
+    被使用的车
+    :param request:
+    :return: list
+    """
+    if request.method == "GET":
+        data = models.Vehicles.objects.filter(status="using").all()
+        email = request.session.get('email')
+        # data = serializers.serialize('json', data)
+        # return HttpResponse(data, content_type="application/json")
+        return render(request, "operator/vehicles_using.html", {"vehicles_using": data, "email": email})
+
+
+def getUnUsingVehicles(request):
+    """
+    未使用的车, move:修改经纬度
+    :param request:
+    :return:
+    """
+    if request.method == "GET":
+        data = models.Vehicles.objects.filter(status="available").all()
+        # data = serializers.serialize('json', data)
+        # return HttpResponse(data, content_type="application/json")
+        email = request.session.get('email')
+        return render(request, "operator/vehicles_available.html", {"vehicles_available": data, "email": email})
+
+
+def getDealVehicles(request):
+    """
+    需要处理的车,charge + repair:broken->available, low battery->available
+    :param request:
+    :return:
+    """
+    if request.method == "GET":
+        data_available = models.Vehicles.objects.filter(status="available").all()
+        data_ex = models.Vehicles.objects.all()
+        vehicles_deal = list(set(data_ex) - set(data_available))
+        return render(request, "operator/vehicles_deal.html", {"vehicles_deal": vehicles_deal})
+
+
+def chargeVehicles(request):
+    if request.method == "POST":
+        apply_type = "charge"
+        vid = request.POST.get("vid")
+        oid = request.session.get('oid')
+        models.Vehicles.objects.filter(id=vid).update(status="available", batteryPercentage=100)
+        models.OperationsHistory.objects.create(operationType=apply_type, oid=oid, vid=vid,
+                                                operateTime=time.strftime('%Y-%m-%d %H:%M:%S'))
+        data = {"success_msg": "success"}
+        return JsonResponse(data)
+
+
+def repairVehicles(request):
+    if request.method == "POST":
+        apply_type = "repair"
+        vid = request.POST.get("vid")
+        locName = request.POST.get("locName")
+        oid = request.session.get('oid')
+        models.Vehicles.objects.filter(id=vid).update(status="available")
+        models.OperationsHistory.objects.create(operationType=apply_type, oid=oid, vid=vid,
+                                                operateTime=time.strftime('%Y-%m-%d %H:%M:%S'))
+        models.RepairHistory.objects.create(repairedLoc=locName, oid=oid, vid=vid,
+                                            repairedTime=time.strftime('%Y-%m-%d %H:%M:%S'))
+        data = {"success_msg": "success"}
+        return JsonResponse(data)
+
+
+def getOperator():
+    oid = requests.Session.get('oid')
+    email = requests.Session.get('email')
+    data = {"success_msg": "success", "oid": oid, "email": email}
+    return JsonResponse(data)
+
+
+def track(request):
+    brokens = models.Vehicles.objects.filter(status="available")
+    low_batterys = models.Vehicles.objects.filter(status="low_battery")
+    availables = models.Vehicles.objects.filter(status="available")
+    usings = models.Vehicles.obkects.filter(status="using")
+
+    return render(request, "/operator/track.html", {"brokens": brokens, "low_batterys": low_batterys,
+                                                    "availables": availables, "usings": usings})
